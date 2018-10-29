f462207d0cfca725f3b49336cdc74c66c686b189
silantyevmn <5992033@mail.ru>
2018-10-29 16:35:50 +0300

    тестовый класс для создания тестового списка

--- app/src/main/java/com/yurentsy/watchingyou/mvp/model/entity/PersonFactory.java
+++ app/src/main/java/com/yurentsy/watchingyou/mvp/model/entity/PersonFactory.java
@@ -1,8 +1,46 @@
 package com.yurentsy.watchingyou.mvp.model.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by silan on 29.10.2018.
  */
 
 public class PersonFactory {
+
+    public static List<Person> getList(){
+        List<Person> personList=new ArrayList<>();
+        personList.add(new Person("2001",
+                "Vladimir",
+                "Putin",
+                "president",
+                "https://www.worldpresidentsdb.com/images/presidents/vladimir-putin.jpg"));
+        personList.add(new Person("2001",
+                "Vladimir",
+                "Putin",
+                "president",
+                "https://www.worldpresidentsdb.com/images/presidents/vladimir-putin.jpg"));
+        personList.add(new Person("2001",
+                "Vladimir",
+                "Putin",
+                "president",
+                "https://www.worldpresidentsdb.com/images/presidents/vladimir-putin.jpg"));
+        personList.add(new Person("2001",
+                "Vladimir",
+                "Putin",
+                "president",
+                "https://www.worldpresidentsdb.com/images/presidents/vladimir-putin.jpg"));
+        personList.add(new Person("2001",
+                "Vladimir",
+                "Putin",
+                "president",
+                "https://www.worldpresidentsdb.com/images/presidents/vladimir-putin.jpg"));
+        personList.add(new Person("2001",
+                "Vladimir",
+                "Putin",
+                "president",
+                "https://www.worldpresidentsdb.com/images/presidents/vladimir-putin.jpg"));
+        return personList;
+    }
 }
