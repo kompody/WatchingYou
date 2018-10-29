7c10188a3cf3c9866c85b40e900edc29d51dc98a
silantyevmn <5992033@mail.ru>
2018-10-29 16:37:55 +0300

    replace     void put(Person person);
        Observable<Person> getPersonById(String id);
    
        Observable<List<Person>> getPersons();

--- app/src/main/java/com/yurentsy/watchingyou/mvp/model/cache/Cache.java
+++ app/src/main/java/com/yurentsy/watchingyou/mvp/model/cache/Cache.java
@@ -1,11 +1,15 @@
 package com.yurentsy.watchingyou.mvp.model.cache;
 
-import com.yurentsy.watchingyou.mvp.model.entity.User;
+import com.yurentsy.watchingyou.mvp.model.entity.Person;
+
+import java.util.List;
 
 import io.reactivex.Observable;
 
 public interface Cache {
-    void putUser(User user);
+    void put(Person person);
 
-    Observable<User> getUser(String id);
+    Observable<Person> getPersonById(String id);
+
+    Observable<List<Person>> getPersons();
 }
