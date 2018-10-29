90ec93a44447eb342bd6cf6fb2e7925e9b3b3a25
silantyevmn <5992033@mail.ru>
2018-10-29 16:30:41 +0300

    relace metod Observable<Person> getPersonById(String id); Observable<List<Person>> getPersons();

--- app/src/main/java/com/yurentsy/watchingyou/mvp/model/repo/Repo.java
+++ app/src/main/java/com/yurentsy/watchingyou/mvp/model/repo/Repo.java
@@ -1,9 +1,13 @@
 package com.yurentsy.watchingyou.mvp.model.repo;
 
+import com.yurentsy.watchingyou.mvp.model.entity.Person;
 import com.yurentsy.watchingyou.mvp.model.entity.User;
+
+import java.util.List;
 
 import io.reactivex.Observable;
 
 public interface Repo {
-    Observable<User> getUser(String id);
+    Observable<Person> getPersonById(String id);
+    Observable<List<Person>> getPersons();
 }
