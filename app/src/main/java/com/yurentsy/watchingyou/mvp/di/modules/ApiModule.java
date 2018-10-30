1ba378e240a4333d066381772a438a1a51c07799
Marat Tlepberghenov <mur.marat@gmail.com>
2018-10-30 11:00:03 +0200

    Fixed base url

--- app/src/main/java/com/yurentsy/watchingyou/mvp/di/modules/ApiModule.java
+++ app/src/main/java/com/yurentsy/watchingyou/mvp/di/modules/ApiModule.java
@@ -27,7 +27,7 @@ public class ApiModule {
     @Provides
     public String baseUrl() {
         // TODO: 28.10.2018 change url
-        return "http://private-15469-employees14.apiary-mock.com/employees";
+        return "http://private-15469-employees14.apiary-mock.com/";
     }
 
     @Provides
