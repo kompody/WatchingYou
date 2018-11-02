f3d9724dc343e1f27a83f2acbfd141491dfd1920
silantyevmn <5992033@mail.ru>
2018-11-03 00:55:39 +0300

    add metod updatePerson

--- app/src/main/java/com/yurentsy/watchingyou/mvp/model/repo/Repo.java
+++ app/src/main/java/com/yurentsy/watchingyou/mvp/model/repo/Repo.java
@@ -1,13 +1,13 @@
 package com.yurentsy.watchingyou.mvp.model.repo;
 
 import com.yurentsy.watchingyou.mvp.model.entity.Person;
-import com.yurentsy.watchingyou.mvp.model.entity.User;
 
 import java.util.List;
 
 import io.reactivex.Observable;
 
 public interface Repo {
-    Observable<Person> getPersonById(String id);
     Observable<List<Person>> getPersons();
+
+    void updatePerson(Person person);
 }
