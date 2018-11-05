b1b74e0dc20086085ac6132c83abba0f84d45917
silantyevmn <5992033@mail.ru>
2018-11-05 21:42:11 +0300

    добавил метод showInfoMessage, который отображает успешную запись в БД

--- app/src/main/java/com/yurentsy/watchingyou/ui/fragment/PersonFragment.java
+++ app/src/main/java/com/yurentsy/watchingyou/ui/fragment/PersonFragment.java
@@ -3,6 +3,7 @@ package com.yurentsy.watchingyou.ui.fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -117,6 +118,11 @@ public class PersonFragment extends MvpAppCompatFragment implements PersonView, 
                 .load(p.getUrlPhoto())
                 .into(photo);
         hideButtons(p.isWorking());
+    }
+
+    @Override
+    public void showInfoMessage(String message) {
+        Snackbar.make(getView(),message,Snackbar.LENGTH_LONG).show();
     }
 
 
