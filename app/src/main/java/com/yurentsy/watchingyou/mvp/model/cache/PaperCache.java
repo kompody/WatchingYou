005340d6eef9fdf660f570dd25ac11528567a382
silantyevmn <5992033@mail.ru>
2018-11-07 16:36:34 +0300

    реализация методов insert,update

--- app/src/main/java/com/yurentsy/watchingyou/mvp/model/cache/PaperCache.java
+++ app/src/main/java/com/yurentsy/watchingyou/mvp/model/cache/PaperCache.java
@@ -66,4 +66,52 @@ public class PaperCache implements Cache {
         });
     }
 
+    @Override
+    public Observable<Boolean> insert(Person p) {
+        return Observable.create(e -> {
+            List<Person> list = Paper.book().read(BASE_KEY);
+            if (list == null) {
+                e.onNext(false);
+            } else {
+                int newID = getMaxId() + 1;
+                p.setID(String.valueOf(newID));
+                list.add(p);
+                Paper.book().write(BASE_KEY, list);
+                e.onNext(true);
+            }
+            e.onComplete();
+        });
+    }
+
+    @Override
+    public Observable<Boolean> delete(Person person) {
+        return Observable.create(e -> {
+            List<Person> list = Paper.book().read(BASE_KEY);
+            if (list == null) {
+                e.onNext(false);
+            } else {
+                list.remove(person);
+                Paper.book().write(BASE_KEY, list);
+                e.onNext(true);
+            }
+            e.onComplete();
+        });
+    }
+
+    private int getMaxId() {
+        List<Person> list = Paper.book().read(BASE_KEY);
+        if (list == null) {
+            return -1;
+        } else {
+            int maxId = 0;
+            for (Person person : list) {
+                int currentId = Integer.parseInt(person.getId());
+                if (currentId > maxId) {
+                    maxId = currentId;
+                }
+            }
+            return maxId;
+        }
+    }
+
 }
