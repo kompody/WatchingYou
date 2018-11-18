0ff79b03fb2e085b063b797af5de3bb993d23ffd
silantyevmn <5992033@mail.ru>
2018-11-18 23:06:51 +0300

    добавил стратегию @StateStrategyType(AddToEndSingleStrategy.class)

--- app/src/main/java/com/yurentsy/watchingyou/mvp/view/PersonEditView.java
+++ app/src/main/java/com/yurentsy/watchingyou/mvp/view/PersonEditView.java
@@ -1,8 +1,11 @@
 package com.yurentsy.watchingyou.mvp.view;
 
 import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 import com.yurentsy.watchingyou.mvp.model.entity.Person;
 
+@StateStrategyType(AddToEndSingleStrategy.class)
 public interface PersonEditView extends MvpView {
     void init();
 
